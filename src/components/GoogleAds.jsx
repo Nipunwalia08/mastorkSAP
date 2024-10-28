f9da5521e8df460e36e15be126eece1274388614
nipun.walia2018@gmail.com <nipun.walia2018@gmail.com>
2024-10-28 22:24:20 +0530

    ads fix

--- src/components/GoogleAds.jsx
+++ src/components/GoogleAds.jsx
@@ -4,19 +4,15 @@ import Script from 'next/script';
 const GoogleAds = () => {
   return (
     <>
-      
-<Script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_ADS_ID}`}>
-</Script>
-<Script>
-  {`
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', '${process.env.NEXT_PUBLIC_ADS_ID}');
-
-  `}
-</Script>
+      <Script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_ADS_ID}`} />
+      <Script id="google-ads">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_ADS_ID}');
+        `}
+      </Script>
     </>
   );
 };
